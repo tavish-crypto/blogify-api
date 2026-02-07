c23701f42cb40096e0ff1f5a0851f674e575054f
tavish-crypto <tavish.modi.s.125@kalvium.community>
2026-02-07 12:26:21 +0530

    make changes in posts and add posts.controller file in controller

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,12 +2,16 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Fetching all blog posts from the modular router!');
-});
+import postController from '../controllers/posts.controller.js';
 
-router.post('/', (req, res) => {
-  res.send('Creating a new blog post...');
-});
+router.get('/', postController.getAllPosts);
+
+// router.get('/', (req, res) => {
+//   res.send('Fetching all blog posts from the modular router!');
+// });
+
+// router.post('/', (req, res) => {
+//   res.send('Creating a new blog post...');
+// });
 
 export default router;
